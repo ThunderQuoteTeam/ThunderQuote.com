b2730843a38d6fb0ee2f3b493e16730d6ca113b6
Fakhzan <fakhzan@thunderquote.com>
2020-11-03 23:53:13 +0800

    Added JSON-LD to index.html, added sitemap.xml and added build.sh bash script to update `thunderquote` and GitHub `ThunderQuote.com` repositories automatically for every build

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Running vue-cli-service build"
+npx vue-cli-service build
+echo "Building complete. Updating /opt/bitnami/apps/thunderquote/mainpage"
+rm -rf /opt/bitnami/apps/thunderquote/mainpage
+cp -r docs /opt/bitnami/apps/thunderquote/mainpage
+echo "Building complete. Updating ThunderQuote.com Repository"
+rm -rf /Users/thunderquote/Documents/GitHub/ThunderQuote.com/docs
+cp -r docs /Users/thunderquote/Documents/GitHub/ThunderQuote.com/docs
+echo "Build script done"
